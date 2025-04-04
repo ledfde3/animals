3c41bca3e98fa5acae01d48f2b46e7b35d4a7ca0
ledfde3 <ayledmafer2807@gmail.com>
2025-04-03 18:12:47 -0700

    Cambios en App.jsx y AnimalsShows.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import{useState} from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal(){
+  const animals =['cat','bird','dog','gator','horse']
+  return animals[Math.floor(Math.random()* animals.length)]
+}
+console.log(getRandomAnimal())
+function App(){
+  const[animals, setAnimals]= useState()
+  const handleClick=()=>{
+    setAnimals([...animals, getRandomAnimal()])
+  }
+  const renderAnimals =animals.map((animal, index)=>{
+    return <AnimalsShow type={animal} key ={index}/>
+  })
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Animals app</h1>
     </>
   )
 }
